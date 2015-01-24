e1e200d4145cb93cb22a30fc180c345a32f13740
gnav <gilles31400@gmail.com>
2015-01-24 22:26:50 +0100

    commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
 	sass : {
 	    options : {
-		
+
 	    },
 	    build : {
 		files : {
@@ -14,10 +14,18 @@ module.exports = function(grunt) {
 	watch : {
 	    files : [ 'assets/sass/*' ],
 	    tasks : [ 'sass' ]
+	},
+	'gh-pages' : {
+	    options : {
+		base : '_site',
+		message : 'Auto-generated commit',
+		branch: 'master'
+	    },
+	    src : [ 'index.html', 'app/**', 'bower_components/**', 'partials/**', 'style/**' ]
 	}
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
-
+    grunt.loadNpmTasks('grunt-gh-pages');
 };
